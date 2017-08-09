081abe8d8dd69262123f6325220d1253dcf2477e
LiChunHua <lichunhua@medatc.com>
2017-08-10 02:21:17 +0800

    x

--- app/catalog/controllers/NewsController.php
+++ app/catalog/controllers/NewsController.php
@@ -13,11 +13,12 @@ class NewsController extends \deepziyu\yii\rest\Controller
      * @param number $simple 是否返回简单结果, 0: 较完整, 1:简单
      * @param number $only_infomaion 是否仅仅资讯
      * @param number $only_hot 是否仅仅热门
+     * @param number $content_len 概要内容长度
      * @param number $page 页码
      * @param number $page_size 分页大小
      * @return [] info 新闻首页数据
      */
-    public function actionList($type_id = 0, $simple = '0', $only_infomaion = '0', $only_hot = '0', $page = 1, $page_size = 15)
+    public function actionList($type_id = 0, $simple = '0', $only_infomaion = '0', $only_hot = '0', $content_len=200, $page = 1, $page_size = 15)
     {
         $search = News::search();
         $search->query->andWhere(['=', 'status', '1']);
@@ -41,14 +42,14 @@ class NewsController extends \deepziyu\yii\rest\Controller
         $search->pagination->page = $page;
         $search->pagination->pageSize = $page_size;
 
-        $items = array_map(function ($d) use ($simple){
+        $items = array_map(function ($d) use ($simple, $content_len){
             $item = [
                 'id' => $d->id,
                 'image' => $d->getImageUrl('news/tmp.jpg')
             ];
 
             if ($simple !== '1') {
-                $item['short_content'] = \common\cms\helper\Content::subString(strip_tags($d->content), 200);
+                $item['short_content'] = \common\cms\helper\Content::subString(strip_tags($d->content), $content_len);
             }
             $item['hits'] = $d->hits;
             $item['created_at'] = $d->created_at;
