4a4737d4b6bb899e8310e55c2cdb4c6179226573
LiChunHua <lichunhua@medatc.com>
2017-08-09 16:09:01 +0800

    x

--- app/member/controllers/ProfileController.php
+++ app/member/controllers/ProfileController.php
@@ -8,7 +8,7 @@ class ProfileController extends \deepziyu\yii\rest\Controller
     /**
      * 获取用户信息
      * @desc 获取用户信息
-     * @return array profile 用户信息
+     * @return [] profile 用户信息
      */
     public function actionGet()
     {
