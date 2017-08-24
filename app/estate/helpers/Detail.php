d30b070a3b0c71ba9259c327fe7145f096bc4bff
LiChunHua <lichunhua@medatc.com>
2017-08-25 00:47:46 +0800

    x

--- app/estate/helpers/Detail.php
+++ app/estate/helpers/Detail.php
@@ -19,12 +19,31 @@ class Detail
                 'EST_ANNUAL_INCOME_CASH' => 0
             ];
         }
-
         $aveResult = WS::$app->db->createCommand('select * from rets_ave_roi where "ZIP_CODE"=:zip', [
                 ':zip' => $zipCode
             ])->queryOne();
 
-        return array_merge($result, $aveResult);
+        $result = array_merge($result, $aveResult);
+        unset($result['ZIP_CODE']);
+        unset($result['AVE_ROI_MORTGAGE']);
+        unset($result['AVE_ANNUAL_INCOME_MORTGAGE']);
+
+        $result['EST_ROI_CASH'] = number_format($result['EST_ROI_CASH'], 2).'%';
+        $result['AVE_ROI_CASH'] = number_format($result['AVE_ROI_CASH'], 2).'%';
+        if (WS::$app->language === 'en-US') {
+            $result['EST_ANNUAL_INCOME_CASH'] = '$'.number_format($result['EST_ANNUAL_INCOME_CASH'], 2);
+            $result['AVE_ANNUAL_INCOME_CASH'] = '$'.number_format($result['AVE_ANNUAL_INCOME_CASH'], 2);
+        } else {
+            $result['EST_ANNUAL_INCOME_CASH'] = number_format($result['EST_ANNUAL_INCOME_CASH'] * 1.0 / 10000, 2).'万美元';
+            $result['AVE_ANNUAL_INCOME_CASH'] = number_format($result['AVE_ANNUAL_INCOME_CASH'] * 1.0 / 10000, 2).'万美元';
+        }
+
+        return [
+            strtolower('EST_ROI_CASH') => $result['EST_ROI_CASH'],
+            strtolower('AVE_ROI_CASH') => $result['AVE_ROI_CASH'],
+            strtolower('EST_ANNUAL_INCOME_CASH') => $result['EST_ANNUAL_INCOME_CASH'],
+            strtolower('AVE_ANNUAL_INCOME_CASH') => $result['AVE_ANNUAL_INCOME_CASH']
+        ];
     }
 
     public static function fetchDetail($rets)
@@ -41,6 +60,7 @@ class Detail
             $r = $e->render();
 
             return [
+                'id' => $e->list_no,
                 'location' => $e->getLocation(),
                 'image' => $e->getPhoto(0, 500, 500),
                 'list_price' => $e->list_price,
