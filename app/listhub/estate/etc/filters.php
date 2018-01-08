e1d6f10417f686e6d696a4fd539edd5e940028e1
LiChunHua <lichunhua@medatc.com>
2018-01-08 14:09:02 +0800

    x

--- app/listhub/estate/etc/filters.php
+++ app/listhub/estate/etc/filters.php
@@ -2,11 +2,8 @@
 $baseFilters = include(dirname(__DIR__).'/../../estate/etc/filters.php');
 
 return array_merge($baseFilters, [
-    'city_code' => function ($cityCode, $query) {
-        // disabled
-    },
     'city_id' => function ($cityId, $query) {
-        $query->andWhere(['=', 'city_id', $cityId]);
+        $query->andWhere('city_id=:city_id or parent_city_id=:city_id', [':city_id' => $cityId]);
     },
     'agrage' => function ($val, $search) {
         // disabled
