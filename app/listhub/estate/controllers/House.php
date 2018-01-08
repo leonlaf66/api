92e4cc2192f883216b065bb2a95c17ffe96fb6bc
LiChunHua <lichunhua@medatc.com>
2018-01-08 13:17:22 +0800

    x

--- app/listhub/estate/controllers/House.php
+++ app/listhub/estate/controllers/House.php
@@ -165,7 +165,7 @@ class House
         $area = \WS::$app->area;
 
         $hotCities = [
-            'ny' => ['Great Nect', 'Jericho', 'Manhasset', 'Syosset', 'New Hyde Park', 'Garden City'],
+            'ny' => ['Great Neck', 'Jericho', 'Manhasset', 'Syosset', 'New Hyde Park', 'Garden City'],
             'il' => ['Chicago', 'Winnetka', 'Hinsdale', 'Clarendon Hills', 'Buffalo Grove'],
             'ga' => ['Atlanta', 'Johns Creek', 'Decatur', 'Alpharetta'],
             'ca' => ['Los Angeles', 'Santa Monica', 'Irvine', 'Temecula', 'San Marino', 'Palos Verdes Estates']
